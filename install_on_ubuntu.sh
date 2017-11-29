5f4de7f0fee0290fc3db3e812993606de39ff6c3
Alex Merkulov <almerico@gmail.com>
2017-11-29 19:10:19 +0200

    Update install_on_ubuntu.sh

--- install_on_ubuntu.sh
+++ install_on_ubuntu.sh
@@ -25,7 +25,9 @@ go get -u github.com/golang/dep/cmd/dep
 dep ensure
 sudo docker swarm init 
 sudo modprobe xt_ipvs
-sudo docker pull  franela/dind:hybrid
+#sudo docker pull  franela/dind:hybrid
+sudo docker pull  franela/dind
+
 mv docker-compose.yml 1.yml;envsubst < "1.yml" > "docker-compose.yml";rm 1.yml
 sed -i  's/- .\/haproxy/- \/home\/ubuntu\/go\/src\/github.com\/play-with-docker\/play-with-docker\/haproxy/g' docker-compose.yml
 sed -i "s/\/pwd\/sessions -name l2/\/pwd\/sessions -name l2 -playground-domain $PLAYGROUNDHOSTNAME/g" docker-compose.yml
